de29b008170b323e74481b28ab7c06a4404d80a5
ZR <44505762@qq.com>
2022-04-29 13:12:38 +0800

    增加节流函数

--- lazyload.js
+++ lazyload.js
@@ -2,19 +2,31 @@ var viewHeight = document.documentElement.clientHeight;
 function lazyloadFuc() {
   var elem = document.querySelectorAll("img[data-original][lazyload]");
   Array.prototype.forEach.call(elem, function (item, index) {
-	var rect;
-	if (item.dataset.original === "") return;
-	rect = item.getBoundingClientRect();
-	if (rect.bottom >= 0 && rect.top < viewHeight) {
-	  let img = new Image();
-	  img.src = item.dataset.original;
-	  img.onload = function () {
-		item.src = img.src;
-		item.removeAttribute("lazyload");
-		item.removeAttribute("data-original");
-	  };
-	}
+    var rect;
+    if (item.dataset.original === "") return;
+    rect = item.getBoundingClientRect();
+    if (rect.bottom >= 0 && rect.top < viewHeight) {
+      let img = new Image();
+      img.src = item.dataset.original;
+      img.onload = function () {
+        item.src = img.src;
+        item.removeAttribute("lazyload");
+        item.removeAttribute("data-original");
+      };
+    }
   });
 }
 lazyloadFuc();
-document.addEventListener("scroll", lazyloadFuc);
+function throttle(fn, delay) {
+  let timer = null;
+  return () => {
+    if (timer) {
+      return;
+    }
+    timer = setTimeout(() => {
+      fn(imgs);
+      timer = null;
+    }, delay);
+  };
+}
+window.onscroll = throttle(lazyloadFuc, 500);
